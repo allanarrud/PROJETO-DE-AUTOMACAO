2d7aa28b6c43a127a90fe725ba9585757169083b
allanarrud <allancrimson@gmail.com>
2025-04-01 13:06:37 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import pyautogui
+from time import sleep
+
+
+# 1 - Clicar e digitar meu usuário
+pyautogui.click(4237,571,duration=2)
+# 2 - Clicar e digitar minha senha
+# 3 - clicar em 'Entrar'
+# 4-  Extrair cada produto
+#  	1 - clicar e digitar produto
+#	2 - clicar e digitar quantidade
+#	3 - clicar e digitar preço
+#	4 - clicar em 'Registrar'"
