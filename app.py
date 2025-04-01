aa70172c5250a9656d52383d4070100e19fa1ab9
allanarrud <allancrimson@gmail.com>
2025-04-01 14:26:09 -0300

    projeto pronto

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import pyautogui
 from time import sleep
 
-
 # 1 - Clicar e digitar meu usuário
-pyautogui.click(4183,655,duration=2)
+pyautogui.click(985,547,duration=2)
 pyautogui.write('beto')
-
 # 2 - Clicar e digitar minha senha
-pyautogui.click(4187,698,duration=2)
+pyautogui.click(1011,584,duration=2)
 pyautogui.write('123456')
-
 # 3 - clicar em 'Entrar'
-pyautogui.click(4039,738,duration=2)
-
-
+pyautogui.click(813,616,duration=2)
 # 4-  Extrair cada produto
 with open('arquivo.txt','r') as arquivo:
     for linha in arquivo:
@@ -21,13 +16,14 @@ with open('arquivo.txt','r') as arquivo:
         quantidade = linha.split(',')[1]
         preco = linha.split(',')[2]
         #1 - clicar e digitar produto
-        pyautogui.click()
+        pyautogui.click(592,516,duration=2)
         pyautogui.write(produto)
         #2 - clicar e digitar quantidade
-        pyautogui.click()
+        pyautogui.click(592,566,duration=2)
         pyautogui.write(quantidade)
         #3 - clicar e digitar preço
-        pyautogui.click()
+        pyautogui.click(592,600,duration=2)
         pyautogui.write(preco)
         #4 - clicar em 'Registrar'
-        pyautogui.click()
+        pyautogui.click(406,833,duration=1)
+        sleep(1)
